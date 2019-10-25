59dd38fa77bbb6f81513590451276ee77bf66891
Adrián Ramírez <adraez.o3@gmail.com>
2019-10-25 17:29:56 +0200

    pacman fondo+fantasma

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ function hola(){
 
 const horizont =  Math.random() * (1260 - 0) 
 const vertical =  Math.random() * (570 - 0) 
-    $(".bicho").css("background-color", "yellow");
+    
     $(".bicho").css("left", horizont);
     $(".bicho").css("top", vertical);
   }
 
 
-  window.setInterval("hola()",2000);
-
+  window.setInterval("hola()",1000);
 
 
 
