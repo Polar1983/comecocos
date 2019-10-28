36c335b57c806a6570240068a16e65158375196f
Adrián Ramírez <adraez.o3@gmail.com>
2019-10-28 12:22:17 +0100

    comecocos v:2.0

--- main.js
+++ main.js
@@ -32,7 +32,6 @@ function phantom(){
 
     const horizont =  Math.random() * (1260 - 0) 
     const vertical =  Math.random() * (570 - 0) 
-        $(".bicho").css("background-color", "yellow");
         $(".bicho").css("left", horizont);
         $(".bicho").css("top", vertical);
       }
